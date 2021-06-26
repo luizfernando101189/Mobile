8e90c5d29264bb3a29089c2d5c264319822f2d15
luizfernando101189 <49911416+luizfernando101189@users.noreply.github.com>
2021-06-26 12:35:06 -0300

    Add files via upload

---
+++ app8_Pet/app/src/main/java/com/luiz/app8_pet/Usuario.kt
@@ -0,0 +1,8 @@
+package com.luiz.app8_pet
+
+import android.graphics.drawable.Drawable
+
+data class Usuario(
+    var foto: Drawable?=null,
+    var nome: String,
+)
---
+++ app8_Pet/app/src/main/java/com/luiz/app8_pet/UsuarioAdapter.kt
@@ -0,0 +1,41 @@
+package com.luiz.app8_pet
+
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.net.toUri
+import androidx.recyclerview.widget.RecyclerView
+import java.net.URI
+
+class UsuarioAdapter(var listaUsuario: MutableList<Usuario>):RecyclerView.Adapter<UsuarioAdapter.ItemViewHolder>() {
+
+    // Responsável por encontrar os componentes dentro do layout (item_usuario) e indicar que o
+    //  layout será replicado na Recycler View
+    class ItemViewHolder(view: View): RecyclerView.ViewHolder(view){
+        val imgFoto: ImageView = view.findViewById(R.id.imgFoto)
+        val txtNome: TextView = view.findViewById(R.id.txtNome)
+    }
+
+    // Responsável por inflar (fazer aparecer) o layout por exemplo na activity ou fragment ou dialog (tudo que extends View Group)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_usuario, parent, false)
+        return ItemViewHolder(view)
+    }
+
+    // Vincula os componentes do layout (item_usuario) a um dado/propriedade/atributo da lista
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        listaUsuario[position].foto?.let{
+            holder.imgFoto.setImageDrawable(it)
+        }
+        holder.txtNome.text = listaUsuario[position].nome
+    }
+
+    // Indica quantos registros/itens a lista possui
+    override fun getItemCount(): Int {
+        return listaUsuario.size
+    }
+}
+
